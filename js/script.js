f63a919a0c37d374685680b1d1527b0e9995cdd4
danim <danimaydana9@gmail.com>
2021-10-24 11:44:59 -0300

    saturday's night update
    se elimanron clases innecesarias, se renombraron otras, se cambio el - por _ en todas las clases, se rediseño la home

--- js/script.js
+++ js/script.js
@@ -1,22 +1,58 @@
 
 
 
-let burger = document.querySelector(".burger"),
+  let pageloader = document.querySelector("#pageloader");
+  let pageLoaderHTML = `
+<div id="pageloader">
+  <div class="loading">
+      <div class="loading-logo">
+          <h1>Viajecitos</h1>
+      </div>
+      <div class="loading-dots">
+          <div class="dot"></div>
+          <div class="dot2"></div>
+          <div class="dot3"></div>
+      </div>
+  </div>
+</div>` ;
 
+
+  if(pageloader){
+      fake_load();
+  }
+  function fake_load(){
+  setTimeout(function(){ pageLoader() }, 1500);
+
+  //document.body.style.overflow = 'hidden'
+
+  function pageLoader() { 
+        pageloader.style.opacity = '0';                 
+        setTimeout(function(){ 
+          pageloader.style.display = 'none';}, 500);
+          //document.body.style.overflow = 'unset'
+
+    };  
+  }
+
+
+
+let burger = document.querySelector(".burger"),
     list_nav = document.querySelector("#nav_menu");
 
 
 burger.addEventListener("click", function() {
       list_nav.classList.toggle('active');
+      burger.classList.toggle('burger_active');
+      document.body.classList.toggle('on_load');
       document.querySelector(".burger__line").classList.toggle('ln_black')
 });
 
 
 /* btn back-top */
 
-var back_top_btn = document.querySelector(".back-top");
+var back_top_btn = document.querySelector(".back_top");
 
-window.onscroll = function () { scrollFunction() ,navbar_scrollUp(), navbarColor() };
+window.onscroll = function () { scrollFunction() ,prev_navbar(), navbarColor() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 ||
@@ -37,88 +73,41 @@ function back_top(){
 
 var prevScrollpos = window.pageYOffset;
 
+
+function prev_navbar(){ 
+  var currentScrollPos = window.pageYOffset;
+  if(currentScrollPos > headerCnHeight){
+     navbar_scrollUp() 
+    } 
+  }
+
 function navbar_scrollUp() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".header_nav").style.top = "0";
   } else {
-    document.querySelector(".header_nav").style.top = "-120px";
+    document.querySelector(".header_nav").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
-/* nav-color */
+
+/* Nav-color */
 
 let header_nav = document.querySelector('.header_nav'),
     headerHeight = header_nav.clientHeight,
+    HeaderCn = document.querySelector('header'),
+    headerCnHeight = HeaderCn.clientHeight,
     altoDeScroll = document.documentElement.scrollTop;
-    header_nav.classList.add("n_fixed")
+header_nav.classList.add("n_fixed")
 
 function navbarColor(){
      let scroll_top = document.documentElement.scrollTop;
-    if ( scroll_top > 0) {
-        header_nav.classList.add("nav_js")
+    if ( scroll_top > 58) {
+        //scroll_top > headerCnHeight - 58
+        header_nav.classList.add("nav_js");
       }
     else{  
         header_nav.classList.remove("nav_js")
     }
 }
 
-
-/** V2
- * 
- * 
- * 
- *  var header_nav = document.querySelector('.header_nav');
-    var headerHeight = header_nav.clientHeight;
-    var altoDeScroll = document.documentElement.scrollTop;
- * 
- * function navbarColor(){
-    //var currenP = document.documentElement.clientHeight;
-     var scroll_top = document.documentElement.scrollTop;
-    if ( scroll_top > 0) {
-        header_nav.style.backgroundColor = "white";
-        header_nav.style.color = 'black';
-        header_nav.style.boxShadow = '0 1px 3px rgb(0 0 0 / 12%), 0 1px 3px rgb(0 0 0 / 2%)';
-        document.querySelector(".main-nav ").style.fontWeight = 'bolder';
-        //console.log('backgroundColor : white');
-      } else {
-        header_nav.style.backgroundColor = "transparent";
-        header_nav.style.color = "white";
-        header_nav.style.boxShadow = 'none';
-        document.querySelector(".main-nav ").style.fontWeight = '100';
-        //console.log('backgroundColor : transparente')
-      }
-     //headerHeight = currentScrollPos;
-}
- * 
- * 
- * 
- */
-
-
-/* copia v1
-
-
-
-function navbarColor(){
-    //var currenP = document.documentElement.clientHeight;
-     var currenP = document.documentElement.scrollTop;
-    if ( currenP > 0) {
-        document.querySelector(".header_nav").style.backgroundColor = "white";
-        document.querySelector(".header_nav").style.color = 'black';
-        document.querySelector(".header_nav").style.boxShadow = '0 1px 3px rgb(0 0 0 / 12%), 0 1px 3px rgb(0 0 0 / 2%)';
-        document.querySelector(".main-nav ").style.fontWeight = 'bolder';
-        console.log('backgroundColor : white');
-      } else {
-        document.querySelector(".header_nav").style.backgroundColor = "transparent";
-        document.querySelector(".header_nav").style.color = "white";
-        document.querySelector(".header_nav").style.boxShadow = 'none';
-        document.querySelector(".main-nav ").style.fontWeight = '100';
-        console.log('backgroundColor : transparente')
-      }
-     //headerHeight = currentScrollPos;
-}
-
-
-
-*/
